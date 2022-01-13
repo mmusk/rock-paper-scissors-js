8ba2877752ae972d70a57f63ed064236cccbb469
Muzakkir <mdmuzakkir296@gmail.com>
2022-01-13 21:08:38 +0530

    created: rock paper scissors game

---
+++ app.js
@@ -0,0 +1,66 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = "ROCK";
+const DRAW = "DRAW";
+const PLAYER_WINS = "PLAYER WON";
+const COMPUTER_WINS = "COMPUTER_WON";
+
+let isGameRunning = false;
+
+const getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}, ''`
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalaid Input we chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === SCISSORS && pChoice === ROCK) ||
+      (cChoice === PAPER && pChoice === SCISSORS)
+    ? PLAYER_WINS
+    : COMPUTER_WINS;
+
+startGameBtn.addEventListener("click", () => {
+  if (isGameRunning) {
+    return;
+  }
+  isGameRunning = true;
+  console.log("Game is starting...");
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
+  const winner = getWinner(computerSelection, playerSelection);
+  console.log(winner);
+
+  let message = `You chose ${playerSelection}, computer chose ${computerSelection}, therefore you `;
+  if (winner === DRAW){
+      message = message + 'had a Draw!'
+  } else if (winner === "PLAYER WON"){
+      message = message + 'Won!'
+  } else{
+      message = message + 'Lost!'
+  }
+  alert(message);
+
+  isGameRunning = false;
+});
